b5f22f200269aa174e1d36cde23071731d6cf470
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2021-12-24 17:43:12 -0300

    build(gradle): don't apply kotlin multiplatform plugin in root module

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlinx.serialization)
 }
 
@@ -11,38 +11,4 @@ version = "0.1.0"
 
 repositories {
     mavenCentral()
-}
-
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
-
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        val jvmMain by getting
-        val jvmTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
-    }
 }
