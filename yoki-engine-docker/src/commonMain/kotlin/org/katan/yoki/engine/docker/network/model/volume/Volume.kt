8c722f8d0ae621436ef0eb9caa1e0c91334e8b27
devwckd <dev.wckd@gmail.com>
2022-01-09 23:13:44 -0300

    feat(docker-engine): documented volume resource

--- yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/network/model/volume/Volume.kt
+++ yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/network/model/volume/Volume.kt
@@ -14,10 +14,33 @@ public data class Volume(
     @SerialName("Options") public val options: Map<String, String>
 )
 
+/**
+ * Filters used in List, Inspect and Prune requests.
+ *
+ * @see org.katan.yoki.engine.docker.resource.volume.VolumeResource.list
+ * @see org.katan.yoki.engine.docker.resource.volume.VolumeResource.inspect
+ * @see org.katan.yoki.engine.docker.resource.volume.VolumeResource.prune
+ */
 public const val VolumeFilters: String = "filters"
 
+/**
+ * Volume definition used in Create request.
+ *
+ * @see org.katan.yoki.engine.docker.resource.volume.VolumeResource.create
+ */
 public const val VolumeDefinition: String = "definition"
 
+/**
+ * Volume's name or ID used in Inspect and Remove requests.
+ *
+ * @see org.katan.yoki.engine.docker.resource.volume.VolumeResource.inspect
+ * @see org.katan.yoki.engine.docker.resource.volume.VolumeResource.remove
+ */
 public const val VolumeName: String = "name"
 
+/**
+ * Parameter used to set if a volume should be force deleted in Remove request
+ *
+ * @see org.katan.yoki.engine.docker.resource.volume.VolumeResource.remove
+ */
 public const val VolumeForceDelete: String = "forceDelete"
--- yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/resource/volume/VolumeResource.kt
+++ yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/resource/volume/VolumeResource.kt
@@ -16,6 +16,14 @@ public class VolumeResource(
     private val engine: DockerEngine
 ) {
 
+    /**
+     * List volumes
+     *
+     * @param options see VolumeList for options.
+     * @return VolumeListResponse
+     *
+     * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeList">VolumeList</a>
+     */
     public suspend fun list(options: Map<String, Any>): VolumeListResponse {
         return engine.httpClient.get("/volumes") {
             options["filter"]?.let {
@@ -24,6 +32,14 @@ public class VolumeResource(
         }.body()
     }
 
+    /**
+     * Create a volume
+     *
+     * @param options see VolumeCreate for options.
+     * @return Volume
+     *
+     * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeCreate">VolumeCreate</a>
+     */
     public suspend fun create(options: Map<String, Any>): Volume {
         require(VolumeDefinition in options) { "Volume Definition is required" }
 
@@ -32,12 +48,27 @@ public class VolumeResource(
         }.body()
     }
 
+    /**
+     * Inspect a volume
+     *
+     * @param options see VolumeInspect for options.
+     * @return Volume
+     *
+     * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeInspect">VolumeInspect</a>
+     */
     public suspend fun inspect(options: Map<String, Any>): Volume {
         require(VolumeName in options) { "Volume Name is required" }
 
         return engine.httpClient.get("/volumes/${options[VolumeName]}").body()
     }
 
+    /**
+     * Remove a volume
+     *
+     * @param options see VolumeDelete for options.
+     *
+     * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeDelete">VolumeDelete</a>
+     */
     public suspend fun remove(options: Map<String, Any>) {
         require(VolumeName in options) { "Volume Name is required" }
 
@@ -48,11 +79,20 @@ public class VolumeResource(
         }
     }
 
+    /**
+     * Delete unused volumes
+     *
+     * @param options see VolumePrune for options.
+     * @return VolumePruneResponse
+     *
+     * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumePrune">VolumePrune</a>
+     */
     public suspend fun prune(options: Map<String, Any>): VolumePruneResponse {
         return engine.httpClient.delete("/volumes/${options[VolumeName]}") {
-            options["filter"]?.let {
+            options[VolumeFilters]?.let {
                 parameter(VolumeFilters, options["filter"])
             }
         }.body()
     }
+
 }
