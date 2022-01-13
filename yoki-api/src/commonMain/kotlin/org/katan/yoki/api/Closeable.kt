3d383ace5a50848a18fe932f53a686664586da72
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-01-13 13:59:14 -0300

    `yoki-api` is now `yoki-core`

--- yoki-api/src/commonMain/kotlin/org/katan/yoki/api/Closeable.kt
+++
@@ -1,7 +0,0 @@
-package org.katan.yoki.api
-
-public expect interface Closeable {
-
-    public fun close()
-
-}
--- yoki-api/src/jvmMain/kotlin/org/katan/yoki/api/Closeable.kt
+++
@@ -1,3 +0,0 @@
-package org.katan.yoki.api
-
-public actual typealias Closeable = AutoCloseable
