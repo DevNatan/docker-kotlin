2ac7bd426a67a05d337d6f43f2f531bc9f3ec0ea
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-01-13 14:29:09 -0300

    Docker config api version documentation

--- yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/DockerConfig.kt
+++ yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/DockerConfig.kt
@@ -2,9 +2,20 @@ package org.katan.yoki.engine.docker
 
 import org.katan.yoki.engine.*
 
+/**
+ * Docker engine configuration
+ *
+ * @see DockerEngine
+ */
 public class DockerEngineConfig : YokiEngineConfig() {
 
+    /**
+     * The version of the Docker API that will be used during communication.
+     *
+     * @see <a href="https://docs.docker.com/engine/api/#versioned-api-and-sdk">Versioned API and SDK</a>
+     */
     public var apiVersion: String = "1.41"
-    public var socketPath: String = "unix://docker.sock"
+
+    public var socketPath: String = "unix:///var/run/docker.sock"
 
 }
