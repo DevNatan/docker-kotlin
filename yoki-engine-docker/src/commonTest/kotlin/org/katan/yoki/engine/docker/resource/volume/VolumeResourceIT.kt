f62a14c1118f03d5fbba8dd4b1eb4edf3b92087c
devwckd <dev.wckd@gmail.com>
2022-01-15 18:10:39 -0300

    test(docker-engine): added tests to volume resource

---
+++ yoki-engine-docker/src/commonTest/kotlin/org/katan/yoki/engine/docker/resource/volume/VolumeResourceIT.kt
@@ -0,0 +1,61 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package org.katan.yoki.engine.docker.resource.volume
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.*
+import org.katan.yoki.engine.docker.*
+import kotlin.test.*
+
+class VolumeResourceIT {
+
+    companion object {
+        const val VOLUME_NAME = "yoki"
+    }
+
+    @Test
+    fun `create network`() = runTest {
+        val client = createYoki()
+        val createdVolume = client.volumes.create { name = VOLUME_NAME }
+        val inspectedVolume = client.volumes.inspect(createdVolume.name)
+        assertEquals(createdVolume.name, inspectedVolume.name)
+
+        // cleanup
+        client.networks.remove(inspectedVolume.name)
+    }
+
+    @Test
+    fun `remove network`() = runTest {
+        val client = createYoki()
+        val network = client.volumes.create { name = VOLUME_NAME }
+        assertTrue(client.volumes.list().volumes.any { it.name == network.name })
+
+        client.volumes.remove(network.name)
+        assertTrue(client.volumes.list().volumes.none { it.name == network.name })
+    }
+
+    @Test
+    fun `list networks`() = runTest {
+        val client = createYoki()
+
+        // the list of networks will never be empty because Docker has predefined networks that cannot be removed
+        assertFalse(client.volumes.list().volumes.isEmpty())
+    }
+
+    @Test
+    fun `prune networks`() = runTest {
+        val client = createYoki()
+        val oldCount = client.volumes.list().volumes.size
+        val newCount = 5
+        repeat(newCount) {
+            client.volumes.create { name = "$VOLUME_NAME-$it" }
+        }
+
+        // check for >= because docker can have default networks defined
+        assertEquals(client.volumes.list().volumes.size, oldCount + newCount)
+
+        client.volumes.prune()
+        assertEquals(client.volumes.list().volumes.size, oldCount)
+    }
+
+}
