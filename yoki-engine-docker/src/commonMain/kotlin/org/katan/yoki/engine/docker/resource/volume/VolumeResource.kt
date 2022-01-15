ed5fd0f5da1faa09531dab7803ccb1a63b8b6ac5
devwckd <dev.wckd@gmail.com>
2022-01-15 16:32:25 -0300

    fix(docker-engine): changed methods to dto instead of string maps

--- yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/resource/volume/VolumeResource.kt
+++ yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/engine/docker/resource/volume/VolumeResource.kt
@@ -3,8 +3,11 @@ package org.katan.yoki.engine.docker.resource.volume
 import io.ktor.client.request.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.katan.yoki.engine.docker.*
 import org.katan.yoki.engine.docker.model.volume.*
+import kotlin.jvm.JvmOverloads
 
 /**
  * @see VolumeResource
@@ -14,6 +17,15 @@ public class VolumeResource(
     private val engine: DockerEngine
 ) {
 
+    private companion object {
+
+        const val BASE_PATH = "/volumes"
+
+        const val LIST_FILTERS = "filters"
+        const val DELETE_FORCE = "force"
+        const val PRUNE_FILTERS = "filters"
+    }
+
     /**
      * List volumes
      *
@@ -22,11 +34,10 @@ public class VolumeResource(
      *
      * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeList">VolumeList</a>
      */
-    public suspend fun list(options: Map<String, Any>): VolumeListResponse {
-        return engine.httpClient.get("/volumes") {
-            options["filter"]?.let {
-                parameter(VolumeFilters, options["filter"])
-            }
+    @JvmOverloads
+    public suspend fun list(filters: VolumeFilters? = null): VolumeListResponse {
+        return engine.httpClient.get(BASE_PATH) {
+            filters?.let { parameter(LIST_FILTERS, Json.Default.encodeToString(it)) }
         }
     }
 
@@ -38,11 +49,10 @@ public class VolumeResource(
      *
      * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeCreate">VolumeCreate</a>
      */
-    public suspend fun create(options: Map<String, Any>): Volume {
-        require(VolumeDefinition in options) { "Volume Definition is required" }
-
-        return engine.httpClient.post("/volumes/create") {
-            body = options[VolumeDefinition]!!
+    @JvmOverloads
+    public suspend fun create(config: VolumeConfig): Volume {
+        return engine.httpClient.post("$BASE_PATH/create") {
+            body = config
         }
     }
 
@@ -54,9 +64,9 @@ public class VolumeResource(
      *
      * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeInspect">VolumeInspect</a>
      */
-    public suspend fun inspect(options: Map<String, Any>): Volume {
-        require(VolumeName in options) { "Volume Name is required" }
-        return engine.httpClient.get("/volumes/${options[VolumeName]}")
+    @JvmOverloads
+    public suspend fun inspect(id: String): Volume {
+        return engine.httpClient.get("$BASE_PATH/${id}")
     }
 
     /**
@@ -66,13 +76,10 @@ public class VolumeResource(
      *
      * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumeDelete">VolumeDelete</a>
      */
-    public suspend fun remove(options: Map<String, Any>) {
-        require(VolumeName in options) { "Volume Name is required" }
-
-        engine.httpClient.delete<Unit>("/volumes/${options[VolumeName]}") {
-            options[VolumeForceDelete]?.let {
-                parameter(VolumeForceDelete, it)
-            }
+    @JvmOverloads
+    public suspend fun remove(id: String, options: VolumeRemove? = null) {
+        engine.httpClient.delete<Unit>("$BASE_PATH/${id}") {
+            options?.let { parameter(DELETE_FORCE, it) }
         }
     }
 
@@ -84,15 +91,57 @@ public class VolumeResource(
      *
      * @see <a href="https://docs.docker.com/engine/api/v1.41/#operation/VolumePrune">VolumePrune</a>
      */
-    public suspend fun prune(options: Map<String, Any>): VolumePruneResponse {
-        return engine.httpClient.delete("/volumes/${options[VolumeName]}") {
-            options[VolumeFilters]?.let {
-                parameter(VolumeFilters, options["filter"])
-            }
+    @JvmOverloads
+    public suspend fun prune(options: VolumePrune? = null): VolumePruneResponse {
+        return engine.httpClient.delete("$BASE_PATH/prune") {
+            options?.let { parameter(PRUNE_FILTERS, options) }
         }
     }
 
 }
+
+public suspend inline fun VolumeResource.list(filters: VolumeFilters.() -> Unit): VolumeListResponse {
+    return list(VolumeFilters().apply(filters))
+}
+
+public suspend inline fun VolumeResource.create(config: VolumeConfig.() -> Unit): Volume {
+    return create(VolumeConfig().apply(config))
+}
+
+public suspend inline fun VolumeResource.remove(id: String, options: VolumeRemove.() -> Unit) {
+    remove(id, VolumeRemove().apply(options))
+}
+
+public suspend inline fun VolumeResource.prune(options: VolumePrune.() -> Unit): VolumePruneResponse {
+    return prune(VolumePrune().apply(options))
+}
+
+@Serializable
+public data class VolumeFilters(
+    public var dangling: Boolean = false,
+    public var driver: String? = null,
+    public var label: String? = null,
+    public var name: String? = null,
+)
+
+public data class VolumeConfig(
+    @SerialName("Name") public var name: String? = null,
+    @SerialName("Driver") public var driver: String? = null,
+    @SerialName("Scope") public var scope: String? = null,
+    @SerialName("Mountpoint") public var mountPoint: String? = null,
+    @SerialName("CreatedAt") public var createdAt: String? = null,
+    @SerialName("Labels") public var labels: Map<String, String>? = null,
+    @SerialName("Options") public var options: Map<String, String>? = null
+)
+
+public data class VolumeRemove(
+    var force: Boolean = false
+)
+
+@Serializable
+public data class VolumePrune(
+    public var label: String? = null
+)
 
 @Serializable
 public data class VolumeListResponse(
