175e8c40d0c05f8bbfa1684586ba5bb6b52c5f8a
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-02-21 22:26:32 -0300

    Fix ExposedPort unresolved Tcp reference

--- yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/model/ExposedPort.kt
+++ yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/model/ExposedPort.kt
@@ -8,5 +8,5 @@ public data class ExposedPort internal constructor(
     public val port: Short
 )
 
-public fun exposedPort(port: Short): ExposedPort = ExposedPort(Tcp, port)
+public fun exposedPort(port: Short): ExposedPort = ExposedPort("tcp", port)
 public fun exposedPort(port: Short, protocol: String): ExposedPort = ExposedPort(protocol, port)
