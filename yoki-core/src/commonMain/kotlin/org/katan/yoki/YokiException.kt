5c0b60086deda1281dd7c445990d36e19b92ac78
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-03-06 14:19:43 -0300

    Create YokiException

---
+++ yoki-core/src/commonMain/kotlin/org/katan/yoki/YokiException.kt
@@ -0,0 +1,8 @@
+package org.katan.yoki
+
+public open class YokiException : Exception {
+    public constructor() : super()
+    public constructor(message: String?) : super(message)
+    public constructor(message: String?, cause: Throwable?) : super(message, cause)
+    public constructor(cause: Throwable?) : super(cause)
+}
