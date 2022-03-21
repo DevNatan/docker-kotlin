5a08301cb8abb3f77d869e88c2100cd9b66235fb
Natan Vieira <natanvnascimento@gmail.com>
2022-03-21 16:31:14 -0300

    Unhandled exceptions should be treated by hand

--- yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/util/Http.kt
+++ yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/util/Http.kt
@@ -16,22 +16,12 @@ private val resourceExceptionJsonDeserializer = Json {
 public expect fun createHttpClient(engine: DockerEngine): HttpClient
 
 // https://stackoverflow.com/a/65579343
-public suspend inline fun <T> HttpClient.requestCatching(
+public inline fun <T> HttpClient.requestCatching(
     block: HttpClient.() -> T,
-    handle: ResponseException.() -> Unit
+    handle: ResponseException.() -> T
 ): Result<T> = runCatching { block() }.recover {
     when (it) {
-        is ResponseException -> {
-            try {
-                // there's a missing else branch in when expression if it continues
-                it.handle()
-            } catch (e: YokiResourceException) {
-                return Result.failure(e)
-            }
-
-            // unhandled exception
-            throw YokiResourceException(it.response.content.readUTF8Line(), it, mapOf())
-        }
+        is ResponseException -> it.handle()
         else -> throw it
     }
 }
@@ -40,7 +30,7 @@ public suspend fun <T : YokiResourceException> ResponseException.throwResourceEx
     factory: (message: String?, cause: Throwable?, properties: Map<String, Any?>) -> T,
     properties: Map<String, Any?>
 ): Nothing {
-    throw factory(response.content.readUTF8Line()?.let {
-        (resourceExceptionJsonDeserializer.decodeFromString(it) as Map<String, @Contextual Any?>)["message"] as? String?
+    throw factory(response.content.readUTF8Line()?.let { content ->
+        (resourceExceptionJsonDeserializer.decodeFromString(content) as Map<String, String?>)["message"]
     }, this, properties)
 }
