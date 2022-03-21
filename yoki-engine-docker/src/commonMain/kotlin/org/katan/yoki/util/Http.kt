627b672ed4d5cc0f6c2a4a5088fa10a71579cf35
Natan Vieira <natanvnascimento@gmail.com>
2022-03-21 16:19:25 -0300

    Catch unhandled ResponseException as YokiResourceException

--- yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/util/Http.kt
+++ yoki-engine-docker/src/commonMain/kotlin/org/katan/yoki/util/Http.kt
@@ -3,16 +3,31 @@ package org.katan.yoki.util
 import io.ktor.client.HttpClient
 import io.ktor.client.features.ResponseException
 import org.katan.yoki.DockerEngine
+import org.katan.yoki.YokiResourceException
+
+private val resourceExceptionJsonDeserializer = Json {
+    ignoreUnknownKeys = true
+}
 
 public expect fun createHttpClient(engine: DockerEngine): HttpClient
 
 // https://stackoverflow.com/a/65579343
-public inline fun <T> HttpClient.requestCatching(
+public suspend inline fun <T> HttpClient.requestCatching(
     block: HttpClient.() -> T,
-    handle: ResponseException.() -> T
+    handle: ResponseException.() -> Unit
 ): Result<T> = runCatching { block() }.recover {
     when (it) {
-        is ResponseException -> it.handle()
+        is ResponseException -> {
+            try {
+                // there's a missing else branch in when expression if it continues
+                it.handle()
+            } catch (e: YokiResourceException) {
+                return Result.failure(e)
+            }
+
+            // unhandled exception
+            throw YokiResourceException(it.response.content.readUTF8Line(), it, mapOf())
+        }
         else -> throw it
     }
 }
