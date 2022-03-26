8045a66ee0e50d759cc1d3a63303d9ee174763fd
Natan Vieira <natanvnascimento@gmail.com>
2022-03-26 18:56:03 -0300

    Force volume removal on tests

--- yoki-engine-docker/src/commonTest/kotlin/org/katan/yoki/engine/docker/TestUtils.kt
+++ yoki-engine-docker/src/commonTest/kotlin/org/katan/yoki/engine/docker/TestUtils.kt
@@ -14,6 +14,7 @@ import org.katan.yoki.resource.container.ContainerCreateOptions
 import org.katan.yoki.resource.container.create
 import org.katan.yoki.resource.volume.VolumeConfig
 import org.katan.yoki.resource.volume.create
+import org.katan.yoki.resource.volume.remove
 import org.katan.yoki.volumes
 import kotlin.test.fail
 
@@ -65,7 +66,9 @@ suspend fun <R> Yoki.withVolume(
     try {
         val volume = volumes.create(config)
         block(volume)
-        volumes.remove(volume.name)
+        volumes.remove(volume.name) {
+            force = true
+        }
     } catch (e: Throwable) {
         fail("Failed to create volume", e)
     }
