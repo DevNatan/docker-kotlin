7fd6bfad6cb0017d3cab682488256c1b595242b2
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-10-30 12:50:43 -0300

    Fix unix socket

--- lib/src/commonMain/kotlin/org/katan/yoki/io/DefaultSockets.kt
+++ lib/src/commonMain/kotlin/org/katan/yoki/io/DefaultSockets.kt
@@ -7,5 +7,5 @@ import kotlin.jvm.JvmName
 internal const val UNIX_SOCKET_PREFIX = "unix://"
 internal const val HTTP_SOCKET_PREFIX = "tcp://"
 
-public const val DEFAULT_DOCKER_UNIX_SOCKET: String = "$UNIX_SOCKET_PREFIX/var/run/docker.sock"
+public const val DEFAULT_DOCKER_UNIX_SOCKET: String = "${UNIX_SOCKET_PREFIX}/var/run/docker.sock"
 public const val DEFAULT_DOCKER_HTTP_SOCKET: String = "${HTTP_SOCKET_PREFIX}localhost:2375"
--- lib/src/jvmMain/kotlin/org/katan/yoki/io/SocketDns.kt
+++ lib/src/jvmMain/kotlin/org/katan/yoki/io/SocketDns.kt
@@ -3,10 +3,12 @@ package org.katan.yoki.io
 import okhttp3.Dns
 import java.net.InetAddress
 
-internal class SocketDns : Dns {
+internal class SocketDns(
+  val isUnixSocket: Boolean,
+) : Dns {
 
     override fun lookup(hostname: String): List<InetAddress> {
-        return if (hostname.endsWith(".socket")) listOf(
+        return if (isUnixSocket) listOf(
             InetAddress.getByAddress(
                 hostname,
                 byteArrayOf(0, 0, 0, 0)
