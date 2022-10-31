3537139fce69e26654191753d74852249825167e
Natan Vieira <itroken99@gmail.com>
2022-10-31 19:14:36 -0300

    Remove executable configuation from native targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,18 +32,10 @@ kotlin {
 
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
+    when {
         hostOs == "Mac OS X" -> macosX64("native")
         isMingwX64 || hostOs == "Linux" -> linuxX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin Native: $hostOs")
-    }
-
-    nativeTarget.apply {
-        binaries {
-            executable {
-                entryPoint = "main"
-            }
-        }
     }
 
     sourceSets {
