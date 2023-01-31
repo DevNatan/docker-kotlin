f8923baea74fd4b1d07615feda5f716325c7577a
devwckd <dev.wckd@gmail.com>
2023-01-31 19:40:20 -0300

    feat: add volumes to ContainerCreateOptions model

--- src/commonMain/kotlin/me/devnatan/yoki/models/container/ContainerCreateOptions.kt
+++ src/commonMain/kotlin/me/devnatan/yoki/models/container/ContainerCreateOptions.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import me.devnatan.yoki.models.HealthConfig
 import me.devnatan.yoki.models.HostConfig
+import me.devnatan.yoki.models.network.NetworkingConfig
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
@@ -28,7 +29,7 @@ public data class ContainerCreateOptions(
     @SerialName("Healthcheck") public var healthcheck: HealthConfig? = null,
     @SerialName("ArgsEscaped") public var escapedArgs: Boolean? = null,
     @SerialName("Image") public var image: String? = null,
-    // TODO volumes
+    @SerialName("Volumes") public var volumes: Map<String, @Contextual Any>? = null,
     @SerialName("WorkingDir") public var workingDirectory: String? = null,
     @SerialName("Entrypoint") public var entrypoint: List<String>? = null,
     @SerialName("NetworkDisabled") public var disabledNetwork: Boolean? = null,
@@ -48,6 +49,10 @@ public fun ContainerCreateOptions.healthcheck(block: HealthConfig.() -> Unit) {
 
 public fun ContainerCreateOptions.hostConfig(block: HostConfig.() -> Unit) {
     this.hostConfig = HostConfig().apply(block)
+}
+
+public fun ContainerCreateOptions.volume(string: String) {
+    this.volumes = mapOf(string to emptyMap<String, Any>()) + volumes.orEmpty()
 }
 
 public fun ContainerCreateOptions.networkingConfig(block: NetworkingConfig.() -> Unit) {
