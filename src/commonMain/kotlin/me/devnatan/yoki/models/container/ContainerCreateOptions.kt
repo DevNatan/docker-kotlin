4ca68256e0d669ee38cf7f3ffc972044e2ffe086
João Victor <65056371+devwckd@users.noreply.github.com>
2023-02-18 17:07:23 -0300

    Add volumes and networking config to container create options (#61)

--- src/commonMain/kotlin/me/devnatan/yoki/models/container/ContainerCreateOptions.kt
+++ src/commonMain/kotlin/me/devnatan/yoki/models/container/ContainerCreateOptions.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import me.devnatan.yoki.models.HealthConfig
 import me.devnatan.yoki.models.HostConfig
+import me.devnatan.yoki.models.network.NetworkingConfig
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
@@ -28,7 +29,7 @@ public data class ContainerCreateOptions(
     @SerialName("Healthcheck") public var healthcheck: HealthConfig? = null,
     @SerialName("ArgsEscaped") public var escapedArgs: Boolean? = null,
     @SerialName("Image") public var image: String? = null,
-    // TODO volumes
+    @SerialName("Volumes") public var volumes: Map<String, @Contextual Any>? = null,
     @SerialName("WorkingDir") public var workingDirectory: String? = null,
     @SerialName("Entrypoint") public var entrypoint: List<String>? = null,
     @SerialName("NetworkDisabled") public var disabledNetwork: Boolean? = null,
@@ -39,7 +40,7 @@ public data class ContainerCreateOptions(
     @SerialName("StopTimeout") public var stopTimeout: Int? = null,
     @SerialName("Shell") public var shell: List<String>? = null,
     @SerialName("HostConfig") public var hostConfig: HostConfig? = null,
-    // TODO networking config
+    @SerialName("NetworkingConfig") public var networkingConfig: NetworkingConfig? = null,
 )
 
 public fun ContainerCreateOptions.healthcheck(block: HealthConfig.() -> Unit) {
@@ -48,6 +49,14 @@ public fun ContainerCreateOptions.healthcheck(block: HealthConfig.() -> Unit) {
 
 public fun ContainerCreateOptions.hostConfig(block: HostConfig.() -> Unit) {
     this.hostConfig = HostConfig().apply(block)
+}
+
+public fun ContainerCreateOptions.volume(string: String) {
+    this.volumes = mapOf(string to emptyMap<String, Any>()) + volumes.orEmpty()
+}
+
+public fun ContainerCreateOptions.networkingConfig(block: NetworkingConfig.() -> Unit) {
+    this.networkingConfig = NetworkingConfig().apply(block)
 }
 
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER")
