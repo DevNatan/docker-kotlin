87cea7dd47f06e940fd7316616fcfc82b21fc009
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-05 15:19:43 -0300

    Remove Windows target

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,12 +36,11 @@ kotlin {
     }
 
     val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin Native: $hostOs")
+    var isNativeUnsupported = false
+    when (hostOs) {
+        "Mac OS X" -> macosX64("native")
+        "Linux" -> linuxX64("native")
+        else -> isNativeUnsupported = true
     }
 
     sourceSets {
@@ -81,15 +80,17 @@ kotlin {
             }
         }
 
-        val nativeMain by getting {
-            dependsOn(commonMain)
-            dependencies {
-                implementation(libs.ktor.client.engine.cio)
+        if (!isNativeUnsupported) {
+            val nativeMain by getting {
+                dependsOn(commonMain)
+                dependencies {
+                    implementation(libs.ktor.client.engine.cio)
+                }
             }
-        }
 
-        val nativeTest by getting {
-            dependsOn(commonTest)
+            val nativeTest by getting {
+                dependsOn(commonTest)
+            }
         }
     }
 }
