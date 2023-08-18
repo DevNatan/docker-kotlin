61499bb78f1c519576080b298e5ed5c310a3e9ae
Natan Vieira <natanvnascimento@gmail.com>
2023-08-18 18:01:48 -0300

    Add `networkSettings` property to container inspection (#107)

--- src/commonMain/kotlin/me/devnatan/yoki/models/container/Container.kt
+++ src/commonMain/kotlin/me/devnatan/yoki/models/container/Container.kt
@@ -3,6 +3,7 @@ package me.devnatan.yoki.models.container
 import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import me.devnatan.yoki.models.network.EndpointSettings
 
 @Serializable
 public data class Container internal constructor(
@@ -26,7 +27,28 @@ public data class Container internal constructor(
     @SerialName("ExecIDs") val execIds: List<String>? = null,
     @SerialName("SizeRw") val sizeRw: Long? = null,
     @SerialName("SizeRootFs") val sizeRootFs: Long? = null,
+    @SerialName("NetworkSettings") val networkSettings: NetworkSettings,
 ) {
 
     val createdAt: Instant by lazy { Instant.parse(createdAtRaw) }
 }
+
+@Serializable
+public data class NetworkSettings internal constructor(
+    @SerialName("Bridge") public val bridge: String,
+    @SerialName("SandboxID") public val sandboxId: String,
+    @SerialName("HairpinMode") public val hairpinMode: Boolean,
+    @SerialName("LinkLocalIPv6Address") public val linkLocalIPv6Address: String,
+    @SerialName("LinkLocalIPv6PrefixLen") public val linkLocalIPv6PrefixLength: Int,
+    @SerialName("GlobalIPv6Address") public val globalIPv6Address: String? = null,
+    @SerialName("GlobalIPv6PrefixLen") public val globalIPv6PrefixLength: Int? = null,
+    @SerialName("IPAddress") public val ipAddress: String? = null,
+    @SerialName("IPPrefixLen") public val ipAddressPrefixLength: Int? = null,
+    @SerialName("IPv6Gateway") public val ipv6Gateway: String? = null,
+    @SerialName("MacAddress") public val macAddress: String? = null,
+    @SerialName("Ports") public val ports: Map<String, Map<String, String>> = emptyMap(),
+    @SerialName("SandboxKey") public val sandboxKey: String,
+    @SerialName("EndpointID") public val endpointId: String,
+    @SerialName("Gateway") public val gateway: String,
+    @SerialName("Networks") public val networks: List<EndpointSettings> = emptyList(),
+)
